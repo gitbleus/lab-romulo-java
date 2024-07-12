9c9b1898e471352df322ca8b4629ece011d27294
bleus <101063772+gitbleus@users.noreply.github.com>
2024-07-12 11:28:33 +0000

    Aula de POO 12/07/2024

---
+++ atividade/Cubo.java
@@ -0,0 +1,5 @@
+public class Cubo{
+    public static void main(String[] args){
+        System.out.println("A");
+    }
+}
--- lab01/Basico1.java
+++ lab01/Basico1.java
@@ -1,9 +1,15 @@
 package lab01;
+import java.util.Scanner;
 
 public class Basico1 {
     // Executando no terminal:
     // java lab01/Basico1.java
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        var sc = new Scanner(System.in);
+        System.out.println("Nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Sobrenome: ");
+        String sobrenome = sc.nextLine();
+        System.out.println(nome + sobrenome);
     }
 }
